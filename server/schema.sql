bb4d1ddaa6985e20590d1c266d124926ac5c5103
Matthew Baseman <matthew.baseman@gmail.com>
2023-04-19 16:00:38 -0700

    Complete designing multi-table schema

--- server/schema.sql
+++ server/schema.sql
@@ -2,13 +2,29 @@ CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE rooms (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  roomname TEXT
+);
+
+CREATE TABLE users (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  username TEXT
+);
+
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  message_text TEXT,
+  roomname INT,
+  username INT,
+  FOREIGN KEY (roomname)
+    REFERENCES rooms(id),
+  FOREIGN KEY (username)
+    REFERENCES users(id)
 );
 
 /* Create other tables and define schemas for them here! */
-
-
 
 
 /*  Execute this file from the command line by typing:
